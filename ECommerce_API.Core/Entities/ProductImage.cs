8096415174ebea57b981335c62b7d1570e34e613
Daniel <d.steinke_98@web.de>
2025-05-14 21:54:31 +0200

    Added ProductImages table

---
+++ ECommerce_API.Core/Entities/ProductImage.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.Domain.Entities
+{
+    public class ProductImage
+    {
+        public Guid ProductImageId { get; set; }
+        public Guid ProductId { get; set; }
+        public string? ImageUrl { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
